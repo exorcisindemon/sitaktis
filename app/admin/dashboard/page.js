8512389e2f168d38d3bb3a961f85ea45a58425ce
sievl <sievl@outlook.com>
2023-10-14 21:10:02 +0700

    update

--- app/admin/dashboard/page.js
+++ app/admin/dashboard/page.js
@@ -1,9 +1,9 @@
-import UserSidebar from "@/components/user/userSidebar";
+import AdminSidebar from "@/components/admin/adminSidebar";
 
 export default function AdminDashboard() {
   return (
     <main className="flex flex-col lg:flex-row w-full h-full lg:h-screen">
-      <UserSidebar />
+      <AdminSidebar />
       <div className="flex flex-col w-full lg:w-[85%] items-center justify-center p-4 gap-8 bg-zinc-100">
         Hi
       </div>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,32 @@
-import UserSidebar from "@/components/user/userSidebar";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <main className="flex flex-col lg:flex-row w-full h-full lg:h-screen">
-      <UserSidebar />
-      <div className="flex flex-col w-full lg:w-[85%] items-center justify-center p-4 gap-8 bg-zinc-100">
-        Hi
+    <main className="bg-zinc-900 flex flex-col lg:flex-row w-full h-screen items-center justify-center">
+      <div className="bg-zinc-100 flex flex-col w-full lg:w-[480px] p-8 rounded-2xl gap-16">
+        <div className="flex flex-col gap-4">
+          <p className="text-zinc-900 font-bold text-2xl">Login</p>
+          <p className="text-zinc-700 font-semibold text-lg">
+            Silahkan masuk terlebih dahulu untuk mengakses dashboard Log Book
+          </p>
+        </div>
+        <form className="flex flex-col gap-4">
+          <input
+            type="text"
+            className="flex w-full bg-zinc-800 px-4 py-3 rounded-lg text-lg transition-all hover:bg-zinc-700"
+            placeholder="NIM"
+          />
+          <input
+            type="password"
+            className="flex w-full bg-zinc-800 px-4 py-3 rounded-lg text-lg transition-all hover:bg-zinc-700"
+            placeholder="Password"
+          />
+        </form>
       </div>
     </main>
   );
